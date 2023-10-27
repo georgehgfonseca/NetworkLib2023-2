6f1fbe913f59eb51d6156575827062db15a8dcc6
George <george.fons@gmail.com>
2023-10-27 19:08:45 -0300

    lesson 1: add edges

--- graph.py
+++ graph.py
@@ -7,6 +7,18 @@ class Graph:
 
   def add_node(self, node):
     if node not in self.adj:
-      self.adj[node] = []
+      self.adj[node] = {}
       self.num_nodes += 1
       
+  def add_edge(self, u, v, weight):
+    if u not in self.adj:
+      self.add_node(u)
+    if v not in self.adj:
+      self.add_node(v)
+    self.adj[u][v] = weight
+    self.num_edges += 1
+
+  def add_two_way_edge(self, u, v, weight):
+    self.add_edge(u, v, weight)
+    self.add_edge(v, u, weight)
+
