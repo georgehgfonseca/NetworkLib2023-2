0059475c97fc79935e35052f904c2ebd262d8f04
George Fonseca <george.fons@gmail.com>
2023-11-01 15:07:29 -0300

    lesson 2: degree-related functions

--- graph.py
+++ graph.py
@@ -22,3 +22,28 @@ class Graph:
     self.add_edge(u, v, weight)
     self.add_edge(v, u, weight)
 
+  def there_is_edge(self, u, v):
+    try:
+      self.adj[u][v]
+      return True
+    except KeyError:
+      return False
+    
+  def degree_out(self, node):
+    return len(self.adj[node])
+  
+  def degree_in(self, node):
+    count = 0
+    for key in self.adj:
+      if node in self.adj[key]:
+        count += 1
+    return count  
+  
+  def neighbors(self, node):
+    return list(self.adj[node].keys())
+
+  def __repr__(self) -> str:
+    str = ""
+    for u in self.adj:
+      str += f"{u} -> {self.adj[u]}\n"
+    return str
