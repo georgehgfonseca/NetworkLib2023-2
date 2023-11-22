26a7042c3472634fd5adb694b87d7360e7ec0bca
George <george.fons@gmail.com>
2023-11-22 14:01:12 -0300

    lesson 4: more exercises and difficulty level

--- graph.py
+++ graph.py
@@ -68,7 +68,7 @@ class Graph:
 
   def there_is_edge(self, u, v) -> bool:
     """
-    Check if there is a directed edge from node u to node v in the graph.
+    [Easy] Check if there is a directed edge from node u to node v in the graph.
 
     Parameters:
     - u: Source node.
@@ -81,7 +81,7 @@ class Graph:
     
   def neighbors(self, node: Any) -> List[Any]:
     """
-    Return a list of neighbor nodes for the given node.
+    [Easy] Return a list of neighbor nodes for the given node.
 
     Parameters:
     - node: The node for which neighbors are to be retrieved.
@@ -93,7 +93,7 @@ class Graph:
 
   def degree_out(self, node: Any) -> int:
     """
-    Return the out-degree of the specified node.
+    [Easy] Return the out-degree of the specified node.
 
     Parameters:
     - node: The node for which the out-degree is to be calculated.
@@ -105,7 +105,7 @@ class Graph:
   
   def degree_in(self, node: Any) -> int:
     """
-    Return the in-degree of the specified node.
+    [Medium] Return the in-degree of the specified node.
 
     Parameters:
     - node: The node for which the in-degree is to be calculated.
@@ -117,7 +117,7 @@ class Graph:
  
   def highest_degree_in(self) -> int:
     """
-    Return the highest in-degree in the graph.
+    [Easy] Return the highest in-degree in the graph.
 
     Returns:
     The highest in-degree in the graph.
@@ -126,7 +126,7 @@ class Graph:
 
   def density(self) -> float:
     """
-    Return the density of the graph.
+    [Easy] Return the density of the graph.
 
     Returns:
     The density of the graph.
@@ -135,7 +135,7 @@ class Graph:
   
   def is_regular(self):
     """
-    Check if the graph is regular.
+    [Easy] Check if the graph is regular.
 
     Returns:
     True if the graph is regular, False otherwise.
@@ -144,7 +144,7 @@ class Graph:
 
   def is_oriented(self):
     """
-    Check if the graph is oriented.
+    [Easy] Check if the graph is oriented.
 
     Returns:
     True if the graph is oriented, False otherwise.
@@ -153,7 +153,7 @@ class Graph:
 
   def is_complete(self) -> bool:
     """
-    Check if the graph is complete.
+    [Easy] Check if the graph is complete.
 
     Returns:
     True if the graph is complete, False otherwise.
@@ -162,7 +162,7 @@ class Graph:
 
   def is_subgraph_of(self, g2) -> bool:
     """
-    Check if the graph is a subgraph of another graph g2.
+    [Medium] Check if the graph is a subgraph of another graph g2.
 
     Parameters:
     - g2: The graph to check against.
@@ -174,7 +174,7 @@ class Graph:
 
   def strongest_connection(self) -> Tuple[Any, Any, float]:
     """
-    Return the edge having the highest weight in the graph.
+    [Easy] Return the edge having the highest weight in the graph.
 
     Returns:
     A tuple (u, v, weight) representing the strongest connection in the graph.
@@ -183,7 +183,7 @@ class Graph:
    
   def weakest_connection(self) -> Tuple[Any, Any, float]:
     """
-    Return the weakest connection in the graph.
+    [Easy] Return the weakest connection in the graph.
 
     Returns:
     A tuple (u, v, weight) representing the weakest connection in the graph.
@@ -192,7 +192,7 @@ class Graph:
 
   def normalize_weights(self) -> None:
     """
-    Normalize the edge weights in the graph.
+    [Medium] Normalize the edge weights in the graph.
 
     This function normalizes the edge weights in the graph to a range between 0 and 1.
     If all weights are the same, a warning is printed.
@@ -201,7 +201,7 @@ class Graph:
 
   def bfs(self, s: Any) -> List[Any]:
     """
-    Perform Breadth-First Search (BFS) starting from the specified source node.
+    [Medium] Perform Breadth-First Search (BFS) starting from the specified source node.
 
     Parameters:
     - s: The source node for the BFS traversal.
@@ -212,7 +212,7 @@ class Graph:
 
   def dfs(self, s: Any) -> List[Any]:
     """
-    Perform Depth-First Search (DFS) starting from the specified source node.
+    [Hard] Perform Depth-First Search (DFS) starting from the specified source node.
 
     Parameters:
     - s: The source node for the DFS traversal.
@@ -223,7 +223,7 @@ class Graph:
 
   def dfs_rec(self, s: Any) -> List[Any]:
     """
-    Perform Recursive Depth-First Search (DFS) starting from the specified source node.
+    [Medium] Perform Recursive Depth-First Search (DFS) starting from the specified source node.
 
     Parameters:
     - s: The source node for the recursive DFS traversal.
@@ -234,7 +234,7 @@ class Graph:
 
   def node_with_highest_degree_in(self) -> Any:    
     """
-    Find and return the node with the highest in-degree in the graph.
+    [Easy] Find and return the node with the highest in-degree in the graph.
 
     Returns:
     The node with the highest in-degree in the graph.
@@ -243,7 +243,7 @@ class Graph:
 
   def node_with_highest_degree_out(self) -> Any:    
     """
-    Find and return the node with the highest out-degree in the graph.
+    [Easy] Find and return the node with the highest out-degree in the graph.
 
     Returns:
     The node with the highest out-degree in the graph.
@@ -252,7 +252,7 @@ class Graph:
 
   def remove_node(self, node: Any) -> None:    
     """
-    Remove the specified node from the graph.
+    [Medium] Remove the specified node from the graph.
 
     Parameters:
     - node: The node to be removed from the graph.
@@ -261,7 +261,7 @@ class Graph:
 
   def remove_directed_edge(self, u: Any, v: Any) -> None:    
     """
-    Remove the directed edge from node 'u' to node 'v' in the graph.
+    [Easy] Remove the directed edge from node 'u' to node 'v' in the graph.
 
     Parameters:
     - u: The source node.
@@ -271,10 +271,104 @@ class Graph:
 
   def remove_undirected_edge(self, u: Any, v: Any) -> None:    
     """
-    Remove the undirected edge between nodes 'u' and 'v' in the graph.
+    [Easy] Remove the undirected edge between nodes 'u' and 'v' in the graph.
 
     Parameters:
     - u: One of the nodes.
     - v: The other node.
+    """
+    pass
+
+  def is_walk(self, nodes: List[any]) -> bool:
+    """
+    [Easy] Check if the sequence of nodes is a valid walk in this graph.
+
+    Parameters:
+    - nodes: Sequecen of nodes.
+
+    Returns:
+    True if nodes is a valid walk, False otherwise.
+    """
+    pass
+
+  def is_path(self, nodes: List[any]) -> bool:
+    """
+    [Medium] Check if the sequence of nodes is a valid path in this graph.
+
+    Parameters:
+    - nodes: Sequecen of nodes.
+
+    Returns:
+    True if nodes is a valid path, False otherwise.
+    """
+    pass
+
+  def is_trail(self, nodes: List[any]) -> bool:
+    """
+    [Medium] Check if the sequence of nodes is a valid trail in this graph.
+
+    Parameters:
+    - nodes: Sequecen of nodes.
+
+    Returns:
+    True if nodes is a valid trail, False otherwise.
+    """
+    pass
+
+  def is_cycle(self, nodes: List[any]) -> bool:
+    """
+    [Medium] Check if the sequence of nodes is a valid cycle in this graph.
+
+    Parameters:
+    - nodes: Sequecen of nodes.
+
+    Returns:
+    True if nodes is a valid cycle, False otherwise.
+    """
+    pass
+
+  def is_connected(self) -> bool:
+    """
+    [Medium] Check if the graph is connected.
+
+    Returns:
+    True if the graph is connected, False otherwise.
+    """
+    pass
+
+  def has_cycle(self) -> bool:
+    """
+    [Hard] Check if the graph has a cycle.
+
+    Returns:
+    True if the graph has a cycle, False otherwise.
+    """
+    pass
+
+  def is_bridge_edge(self, edge: (any, any)) -> bool:
+    """
+    [Hard] Check if the given edge is a bridge in this graph.
+
+    A bridge edge is one whose removal would disconnect the graph.
+
+    Parameters:
+    - edge: Pair of nodes.
+
+    Returns:
+    True if edge is a bridge, False otherwise.
+    """
+    pass
+
+  def is_linking_node(self, node: any) -> bool:
+    """
+    [Hard] Check if the given node is a linking node in this graph.
+
+    A linking node is one whose removal would disconnect the graph.
+
+    Parameters:
+    - node: Node to check.
+
+    Returns:
+    True if node is a linking node, False otherwise.
     """
     pass
